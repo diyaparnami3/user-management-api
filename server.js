444d8f3268c2fad5507d6c4c6c827ff8a2af225c
diyaparnami3 <diyaparnami3126@gmail.com>
2026-02-12 17:05:01 +0530

    Clean initial commit

---
+++ server.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+/* ========= CONNECT DATABASE ========= */
+
+mongoose.connect("mongodb+srv://diya:diya123@cluster0.dous9by.mongodb.net/usersDB")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+/* ========= USER MODEL ========= */
+
+const userSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+/* ========= ROOT ========= */
+
+app.get("/", (req,res)=>{
+  res.send("User API running with MongoDB");
+});
+
+/* ========= CREATE USER ========= */
+
+app.post("/users", async (req,res)=>{
+  try{
+    const {name,email} = req.body;
+
+    if(!name || !email){
+      return res.status(400).json({
+        message:"Name and email required"
+      });
+    }
+
+    const user = await User.create({name,email});
+    res.status(201).json(user);
+
+  }catch(err){
+    res.status(500).json({
+      message:"Server error"
+    });
+  }
+});
+
+/* ========= GET ALL USERS ========= */
+
+app.get("/users", async (req,res)=>{
+  const users = await User.find();
+  res.json(users);
+});
+
+/* ========= GET USER BY ID ========= */
+
+app.get("/users/:id", async (req,res)=>{
+  const user = await User.findById(req.params.id);
+
+  if(!user){
+    return res.status(404).json({
+      message:"User not found"
+    });
+  }
+
+  res.json(user);
+});
+
+/* ========= UPDATE USER ========= */
+
+app.put("/users/:id", async (req,res)=>{
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new:true}
+  );
+
+  if(!user){
+    return res.status(404).json({
+      message:"User not found"
+    });
+  }
+
+  res.json(user);
+});
+
+/* ========= DELETE USER ========= */
+
+app.delete("/users/:id", async (req,res)=>{
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if(!user){
+    return res.status(404).json({
+      message:"User not found"
+    });
+  }
+
+  res.json({
+    message:"User deleted successfully"
+  });
+});
+
+/* ========= START SERVER ========= */
+
+app.listen(3000, ()=>{
+  console.log("Server started on port 3000");
+});
