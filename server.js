503fd8c68e0d4da39c87e2eaf052e9c954583299
diyaparnami3 <diyaparnami3126@gmail.com>
2026-02-12 21:16:08 +0530

    connected database properly

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ app.use(express.json());
 /* ========= CONNECT DATABASE ========= */
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+.then(() => {
+    console.log(" MongoDB Connected");
+})
+.catch((err) => {
+    console.log(" MongoDB Error:", err);
+});
 
 /* ========= USER MODEL ========= */
 
@@ -33,9 +37,9 @@ app.get("/", (req,res)=>{
 });
 
 /* ========= CREATE USER ========= */
-
 app.post("/users", async (req,res)=>{
   try{
+
     const {name,email} = req.body;
 
     if(!name || !email){
@@ -45,20 +49,32 @@ app.post("/users", async (req,res)=>{
     }
 
     const user = await User.create({name,email});
+
     res.status(201).json(user);
 
   }catch(err){
+
+    console.log("CREATE USER ERROR 👉", err); // ⭐ VERY IMPORTANT
+
     res.status(500).json({
-      message:"Server error"
+      message: err.message
     });
   }
 });
 
+
+
 /* ========= GET ALL USERS ========= */
 
+
 app.get("/users", async (req,res)=>{
-  const users = await User.find();
-  res.json(users);
+  try{
+    const users = await User.find();
+    res.json(users);
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message:"Error fetching users"});
+  }
 });
 
 /* ========= GET USER BY ID ========= */
